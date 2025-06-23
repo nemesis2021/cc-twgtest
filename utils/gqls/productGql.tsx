a44e52f94a764a480b2fc62c6d4b03ec84f68546
ministry-of-media <deanlilly@ministryofmedia.co.nz>
2025-06-23 16:07:51 +1200

    Revert productGql.tsx

--- utils/gqls/productGql.tsx
+++ utils/gqls/productGql.tsx
@@ -99,11 +99,6 @@ const PRODUCT_GQL = `
         label
         text
       }
-      ... on ProductFieldsProductInfoFreeTextBlock2Layout {
-        fieldGroupName
-        label
-        text
-      }
       ... on ProductFieldsProductInfoEquationBlockLayout {
         fieldGroupName
         label
